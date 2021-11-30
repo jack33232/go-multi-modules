dea7e2f7e82f3a75d0c2f35c016ca2e4d893cf78
jzhuo3 <jack33232@gmail.com>
2021-11-30 22:28:01 +0800

    - verify major version

--- mod1/go.mod
+++ mod1/go.mod
@@ -1,3 +1,3 @@
-module github.com/jack33232/go-multi-modules/mod1
+module github.com/jack33232/go-multi-modules/mod1/v2
 
 go 1.17
--- mod1/mod1.go
+++ mod1/mod1.go
@@ -3,5 +3,5 @@ package mod1
 import "fmt"
 
 func Hello() {
-	fmt.Println("Hello World from MOD1")
+	fmt.Println("Hello World from MOD1 V2")
 }
